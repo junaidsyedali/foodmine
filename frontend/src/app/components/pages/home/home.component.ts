14e3c44586b3e90c13b38bffa18bbf1ff3f43448
Junaid <junaidsyedali2000@gmail.com>
2025-12-26 17:46:42 -0500

    added search bar functionality

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
 import { Food } from '../../../shared/models/food';
 import { FoodService } from '../../../services/food.service';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { StarRatingComponent } from '../../partials/star-rating/star-rating.component';
+import { SearchComponent } from '../../partials/search/search.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, CommonModule, StarRatingComponent],
+  imports: [RouterLink, CommonModule, StarRatingComponent, SearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   foods: Food[] = [];
 
-  constructor(private foodService: FoodService) {
-    this.foods = foodService.getAll();
+  constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
+    activatedRoute.params.subscribe((params) => {
+      if (params['searchTerm']) {
+        this.foods = foodService.getAllFoodBySearchTerm(params['searchTerm']);
+      } else {
+        this.foods = foodService.getAll();
+      }
+    });
   }
 }
--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -9,4 +9,10 @@ export class FoodService {
   getAll(): Food[] {
     return sample_foods;
   }
+
+  getAllFoodBySearchTerm(searchTerm: string): Food[] {
+    return this.getAll().filter((food) =>
+      food.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
 }
