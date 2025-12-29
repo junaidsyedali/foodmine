a9bc4775e360a39ca955cdb8088b8b60d7d608ce
Junaid <junaidsyedali2000@gmail.com>
2025-12-29 18:36:40 -0500

    added express routers, connected to mongodb, updated api routes

---
+++ backend/src/routers/user.router.ts
@@ -0,0 +1,50 @@
+import { Router } from "express";
+import { sample_users } from "../data";
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import { UserModel } from "../models/user.model";
+
+const router = Router();
+
+router.get(
+  "/seed",
+  asyncHandler(async (req, res) => {
+    const usersCount = await UserModel.countDocuments();
+    if (usersCount > 0) {
+      res.send("Seed is already done");
+      return;
+    }
+    await UserModel.create(sample_users);
+    res.send("Seed is done!");
+  })
+);
+
+router.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email: email, password: password });
+    if (user) {
+      res.send(generateTokenResponse(user));
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
+    }
+  })
+);
+
+const generateTokenResponse = (user: any) => {
+  const token = jwt.sign(
+    {
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    "secretkey",
+    {
+      expiresIn: "30d",
+    }
+  );
+  user.token = token;
+  return user;
+};
+
+export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import cors from "cors";
-import { sample_foods, sample_tags, sample_users } from "./data";
-import jwt from "jsonwebtoken";
+import foodRouter from "./routers/food.router";
+import userRouter from "./routers/user.router";
+import { dbConnect } from "./configs/config";
+import dotenv from "dotenv";
+dotenv.config();
+
+dbConnect();
 
 const app = express();
 app.use(express.json());
@@ -9,62 +14,8 @@ const PORT = 5000;
 app.use(cors({ credentials: true, origin: ["http://localhost:4200"] }));
 app.disable("etag");
 
-app.get("/api/foods", (req, res) => {
-  res.json(sample_foods);
-});
-
-app.get("/api/foods/tags", (req, res) => {
-  res.json(sample_tags);
-});
-
-app.get("/api/foods/:foodId", (req, res) => {
-  const foodId = req.params.foodId;
-  const food = sample_foods.find((food) => food.id === foodId);
-  res.json(food);
-});
-
-app.get("/api/foods/search/:searchTerm", (req, res) => {
-  const searchTerm = req.params.searchTerm;
-  const filteredFoods = sample_foods.filter((food) =>
-    food.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-  res.json(filteredFoods);
-});
-
-app.get("/api/foods/tag/:tagName", (req, res) => {
-  const tagName = req.params.tagName;
-  const filteredFoods = sample_foods.filter((food) =>
-    food.tags.includes(tagName)
-  );
-  res.json(filteredFoods);
-});
-
-app.post("/api/users/login", (req, res) => {
-  const { email, password } = req.body;
-  const user = sample_users.find(
-    (user) => user.email === email && user.password === password
-  );
-  if (user) {
-    res.send(generateTokenResponse(user));
-  } else {
-    res.status(401).json({ message: "Invalid email or password" });
-  }
-});
-
-const generateTokenResponse = (user: any) => {
-  const token = jwt.sign(
-    {
-      email: user.email,
-      isAdmin: user.isAdmin,
-    },
-    "secretkey",
-    {
-      expiresIn: "30d",
-    }
-  );
-  user.token = token;
-  return user;
-};
+app.use("/api/foods", foodRouter);
+app.use("/api/users", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
